4bb8de0e6f18f066aa7cc42d90c1a018f300908d
Ertmin <ertmin_93@live.dk>
2012-12-02 14:22:59 +0100

    app status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = 'ab0c6fbedfbc847e69903c80708435d7a946bc862e27a9637bd40cbf7479c87053f73ac096892e15bf135c6e5356191e15371999b3a76acfaf487bf7620d4111'
